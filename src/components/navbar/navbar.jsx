8a0048c915458336cce45f0fc75425b6fe7a722f
Nafran-Naf <mohamednafran0123@gmail.com>
2025-05-19 00:53:51 +0530

    navbar added

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import { FaSpotify, FaHome, FaSearch, FaBox } from 'react-icons/fa';
+import { FiDownload } from 'react-icons/fi';
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div>
-      <nav>
-        navbar
-      </nav>
-    </div>
-  )
-}
+    <nav className="spotify-navbar">
+      <div className="left-section">
+        <FaSpotify className="spotify-icon" />
+        <FaHome className="home-icon" />
 
-export default navbar;
+        <div className="search-bar">
+          <FaSearch className="search-icon" />
+          <input type="text" placeholder="What do you want to play?" />
+          <FaBox className="box-icon" />
+        </div>
+      </div>
 
+      <div className="right-section">
+        <span className='right-Text'>Premium</span>
+        <span className='right-Text'>Support</span>
+        <span className='right-Text'>Download</span>
+        <div className="divider" />
+        <span className="install-app text">
+          <FiDownload className="download-icon" /> Install App
+        </span>
+        <span className='text'>Sign up</span>
+        <button className="login-button">Log in</button>
+      </div>
+    </nav>
+  );
+};
 
+export default Navbar;
